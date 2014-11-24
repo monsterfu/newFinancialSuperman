dcfa675227dbbcf9fca33f34ab8854828ed2608a
monsterfu <seanh@seanHtekiMac-mini.local>
2014-11-24 21:45:51 +0800

    sss

---
+++ FinancialSuperman/category/UIButton+reversal/UIButtonImageOrder+reversal.m
@@ -0,0 +1,24 @@
+//
+//  UIButton+reversal.m
+//  FinancialSuperman
+//
+//  Created by Monster on 14-11-1.
+//  Copyright (c) 2014年 Monster. All rights reserved.
+//
+
+#import "UIButtonImageOrder+reversal.h"
+
+#define DEGREES_TO_RADIANS(angle) ((angle) / 180.0 * M_PI)
+
+@implementation UIButtonImageOrder (reversal)
+-(void)reversal
+{
+    if (self.normalOrder) {
+        self.imageView.transform=CGAffineTransformMakeRotation(DEGREES_TO_RADIANS(180.0));
+        self.normalOrder = NO;
+    }else{
+        self.imageView.transform=CGAffineTransformMakeRotation(DEGREES_TO_RADIANS(0.0));
+        self.normalOrder = YES;
+    }
+}
+@end
