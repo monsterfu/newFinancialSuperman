aee0321e806b463dab46c26851cff2af4ff83726
Monsterfu <monsterfu@ivmall.com>
2014-11-26 18:09:34 +0800

    add processView

---
+++ FinancialSuperman/views/processView.h
@@ -0,0 +1,17 @@
+//
+//  processView.h
+//  FinancialSuperman
+//
+//  Created by Monster on 14-11-26.
+//  Copyright (c) 2014年 Monster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface processView : UIView
+{
+    
+}
+
+
+@end
---
+++ FinancialSuperman/views/processView.m
@@ -0,0 +1,21 @@
+//
+//  processView.m
+//  FinancialSuperman
+//
+//  Created by Monster on 14-11-26.
+//  Copyright (c) 2014年 Monster. All rights reserved.
+//
+
+#import "processView.h"
+
+@implementation processView
+
+
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+
+
+@end
